827e528aec20a532f6554589cf18e8fe35bfda58
RebeccaHA <rebeccahunt034@gmail.com>
2021-08-17 11:39:13 +0100

    correct naming

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="flex flex-center">
-    <table></table>
+    <Table></Table>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import table from "../components/Table.vue";
+import Table from "../components/Table";
 
 export default defineComponent({
   name: "PageIndex",
