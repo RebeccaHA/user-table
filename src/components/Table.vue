6985dcea3a396b684f30ed3e484a8732bb112445
RebeccaHA <rebeccahunt034@gmail.com>
2021-08-17 11:26:52 +0100

    set up table component

---
+++ src/components/Table.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Table"
+});
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <table></table>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import Table from "../components/Table.vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  name: "PageIndex"
+});
 </script>
