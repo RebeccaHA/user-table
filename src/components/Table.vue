10378a326b573f76e27edd2e96f8cee9ebb34f7d
RebeccaHA <rebeccahunt034@gmail.com>
2021-08-17 12:06:54 +0100

    set up api call and populated table data

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
-    hello
+    <q-table title="Users" :columns="columns" row-key="name" :rows="this.rows">
+    </q-table>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import axios from "axios";
 
 export default defineComponent({
-  name: "Table"
+  name: "Table",
+  data() {
+    return {
+      columns: [
+        {
+          name: "name",
+          label: "Name",
+          field: "name",
+          sortable: true,
+          align: "left"
+        },
+        { name: "email", label: "Email", field: "email", sortable: true },
+        { name: "company", label: "Company", field: "company", sortable: true },
+        { name: "added", label: "Added", field: "added", sortable: true }
+      ],
+      rows: []
+    };
+  },
+  mounted() {
+    axios
+      .get("https://renewable.exchange/test/users.json")
+      .then(response => (this.rows = response.data));
+  }
 });
 </script>
