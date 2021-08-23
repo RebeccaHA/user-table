36eed96094a8a0922235d8240b2aa3049d5af0df
RebeccaHA <rebeccahunt034@gmail.com>
2021-08-23 21:09:18 +0100

    tidied code

--- src/store/usersData/mutations.js
+++ src/store/usersData/mutations.js
@@ -9,11 +9,4 @@ export function addUsers(state, data) {
 export function deleteUsers(state, data) {
   const match = state.users.filter(user => data.includes(user));
   return (state.users = state.users.filter(i => !match.includes(i)));
-
-  // const matchID = state.users.map(user => {
-  //   if (user.name === match.name) {
-  //     state.users.indexOf(user);
-  //   }
-  // });
-  // state.users.splice(matchID, );
 }
